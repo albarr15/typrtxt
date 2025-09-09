7c1d768b60dcb627d83624fdeaddb21091bcd574
clarissaalbarr <cmalbarracin15@gmail.com>
2025-09-10 00:28:57 +0800

    Added automatic class sorting with Prettier

--- typr/src/App.vue
+++ typr/src/App.vue
@@ -2,16 +2,16 @@
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
   <p>
-    Visit <a class="underline" href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit
+    <a class="underline" href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
   </p>
-  <br>
-  <p class="text-3xl font-bold underline"> trial text</p>
+  <br />
+  <p class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">trial text</p>
+  <div class="p-4 pt-2"><!-- ... --></div>
 </template>
 
 <style scoped></style>
